dd6472377e9f9d8698fd6fa680186fbfcb250c29
Zilin-Chen-22 <3485087383@qq.com>
2024-12-07 15:29:37 -0500

    Add files via upload
    Now the planner can solve multiple gates problem

---
+++ define.py
@@ -0,0 +1,70 @@
+# from math import pi
+import math
+import matplotlib.pyplot as plt
+
+class Gate:
+    def __init__(self, x, y, z, angle,size):
+        self.x = x
+        self.y = y
+        self.z = z
+        self.angle = (angle - 90) * math.pi / 180
+        self.size = size     # unit: m
+        self.edge_1_x = self.x + self.size / 2 * math.cos(self.angle)
+        self.edge_2_x = self.x - self.size / 2 * math.cos(self.angle)
+        self.edge_1_y = self.y + self.size / 2 * math.sin(self.angle)
+        self.edge_2_y = self.y - self.size / 2 * math.sin(self.angle)
+
+class Traj:
+    passGateTime = []
+    switchTime = []
+
+    def __init__(self, time, p_x, p_y, p_z, gates):
+        self.time = time
+        self.p_x = p_x
+        self.p_y = p_y
+        self.p_z = p_z
+        self.gates = gates
+
+        gates_count = 0
+
+        for t in range(1, len(self.time)):
+            l1x1 = self.p_x[t - 1]
+            l1y1 = self.p_y[t - 1]
+            l1x2 = self.p_x[t]
+            l1y2 = self.p_y[t]
+            l2x1 = self.gates[gates_count].edge_1_x
+            l2y1 = self.gates[gates_count].edge_1_y
+            l2x2 = self.gates[gates_count].edge_2_x
+            l2y2 = self.gates[gates_count].edge_2_y
+
+            if (max(l1x1, l1x2) < min(l2x1, l2x2)
+                or max(l1y1, l1y2) < min(l2y1, l2y2)
+                or max(l2x1, l2x2) < min(l1x1, l1x2)
+                or max(l2y1, l2y2) < min(l1y1, l1y2)):
+                continue
+            elif (((l1x1 - l2x1) * (l2y2 - l2y1) - (l1y1 - l2y1) * (l2x2 - l2x1)) * 
+                  ((l1x2 - l2x1) * (l2y2 - l2y1) - (l1y2 - l2y1) * (l2x2 - l2x1)) <= 0 or
+                  ((l2x1 - l1x1) * (l1y2 - l1y1) - (l2y1 - l1y1) * (l1x2 - l1x1)) *
+                  ((l2x2 - l1x1) * (l1y2 - l1y1) - (l2y2 - l1y1) * (l1x2 - l1x1)) <= 0):
+                self.passGateTime.append(t)
+                self.switchTime.append(t - 10)
+                gates_count += 1
+                if gates_count >= len(self.gates):
+                    break
+        self.switchTime.append(len(self.time) - 1)
+        if self.time[0] == 0:
+            self.time[0] = 0.0000001
+
+    def plot_2D(self):
+        # first plot traj
+        plt.plot(self.p_x, self.p_y)
+        plt.title("MAP")
+
+        # then plot the gates
+        for i in range(len(self.gates)):
+            plt.plot([self.gates[i].edge_1_x, self.gates[i].edge_2_x], [self.gates[i].edge_1_y, self.gates[i].edge_2_y], 'k')
+        
+        # show the point that the drone is flying across a gate
+        for i in self.passGateTime:
+            plt.scatter(self.p_x[i], self.p_y[i], color = 'r', s = 100)
+        plt.show()
--- yaw_planning.py
+++ yaw_planning.py
@@ -1,57 +1,104 @@
 from casadi import *
 import matplotlib.pyplot as plt
+import define as map
 
 # Suming rounds for approximate to an Intergral
 ROUNDS = 2000   # default value: 2000 times
-LOOPTIME = 1  # Unit: ms
+GATES_NUM = 0
+MAX_TIME_ROUNDS = 0
+# LOOPTIME = 1  # Unit: ms
 
 # unit rad/s
-W_MAX = 5   
+W_MAX = 5  
 W_MIN = -W_MAX
 A_MAX = 10
 A_MIN = -A_MAX
 
-def p2p_plan(w0, theta0, T, gate_x, gate_y):
+def p2p_plan(w0, theta0, traj, gate):
     global ROUNDS
-    ROUNDS = int(T * (10 ** 3) / LOOPTIME)
+    ROUNDS = len(traj.time)
     # a0 = A_MIN  
     opti = Opti()
-
+    
     alpha = opti.variable()
     beta = opti.variable()
     gamma = opti.variable()
     
     # Assume the acceleration can change immediately at the starting point
-    a0 = opti.variable()    # Starting point acceleration in rad/s(-2)  
+    a0 = opti.variable()    # Starting point acceleration in rad/s^(-2)  
     
     # t = opti.variable()
 
-    opti.minimize( f_x(alpha, beta, gamma, a0, w0, theta0, T, gate_x, gate_y) )
+    opti.minimize( f1_x(alpha, beta, gamma, a0, w0, theta0, traj, gate.x, gate.y) )
 
-    for i in range(1, ROUNDS + 1):
-        t = i * T / ROUNDS
+    for i in range(ROUNDS):
+        t = traj.time[i]
 
-        opti.subject_to( g1_x(alpha, beta, gamma, a0, w0, t))
+        if t == 0:
+            t = 0.00001
+
+        opti.subject_to( g1_x(alpha, beta, gamma, a0, w0, t) )
         opti.subject_to( h1_x(alpha, beta, gamma, a0, t) )
-        # opti.subject_to( W_MIN <= (alpha * (t ** 4) / 24 + beta * (t ** 3) / 6 + gamma * (t ** 2) / 2 + a0 * t + w0) <= W_MAX )
-        # opti.subject_to( j1_x(t, T) )
         opti.subject_to( g2_x(alpha, beta, gamma, a0, w0, t) )
         opti.subject_to( h2_x(alpha, beta, gamma, a0, t) )
-        # opti.subject_to( i2_x(alpha, beta, gamma, a0, w0, theta0, T) )
-        # opti.subject_to( j2_x(t, T) )
 
-    dx = (gate_x - x_pos(T))
-    dy = (gate_y - y_pos(T))
+    T = traj.time[-1]
+
+    dx = (gate.x - traj.p_x[-1])
+    dy = (gate.y - traj.p_y[-1])
     if dx == 0 and dy == 0:
-        dx = (gate_x - x_pos(T - T / ROUNDS))
-        dy = (gate_y - y_pos(T - T / ROUNDS))
+        dx = (gate.x - traj.p_x[-2])
+        dy = (gate.y - traj.p_y[-2])
+        last_target = atan(dy / dx)
     elif dx == 0 and dy > 0:
         last_target = pi / 2
     elif dx == 0 and dy < 0:
         last_target = - pi / 2
     else:
         last_target = atan(dy / dx)
-    opti.subject_to( theta(alpha, beta, gamma, a0, w0, theta0, T) == last_target)
+    opti.subject_to(theta(alpha, beta, gamma, a0, w0, theta0, T) == last_target)
+
+    opti.solver('ipopt')
+
+    sol = opti.solve()
+
+    return [sol.value(alpha), sol.value(beta), sol.value(gamma), sol.value(a0)]
+
+def gates_plan(w0, theta0, traj, gate):
+    global MAX_TIME_ROUNDS
+    MAX_TIME_ROUNDS = len(traj.time)
+    # a0 = A_MIN  
+    opti = Opti()
+    
+    alpha = opti.variable()
+    beta = opti.variable()
+    gamma = opti.variable()
+    
+    # Assume the acceleration can change immediately at the starting point
+    a0 = opti.variable()    # Starting point acceleration in rad/s^(-2)  
+    
+    # t = opti.variable()
+
+    opti.minimize( f2_x(alpha, beta, gamma, a0, w0, theta0, traj, gate.x, gate.y) )
+
+    for i in range(MAX_TIME_ROUNDS):
+        t = traj.time[i]
+
+        if t == 0:
+            t = 0.00001
+
+        opti.subject_to( g1_x(alpha, beta, gamma, a0, w0, t) )
+        opti.subject_to( h1_x(alpha, beta, gamma, a0, t) )
+        opti.subject_to( g2_x(alpha, beta, gamma, a0, w0, t) )
+        opti.subject_to( h2_x(alpha, beta, gamma, a0, t) )
+
+    # T = traj.time[-1]
+
+    # dx = (gate.x - traj.p_x[-1])
+    # dy = (gate.y - traj.p_y[-1])
+    # ds = sqrt(dx ** 2 + dy ** 2)
+    # last_target = asin(dy / ds)
+    # opti.subject_to(theta(alpha, beta, gamma, a0, w0, theta0, T) == last_target)
 
     opti.solver('ipopt')
 
@@ -60,16 +107,52 @@ def p2p_plan(w0, theta0, T, gate_x, gate_y):
     return [sol.value(alpha), sol.value(beta), sol.value(gamma), sol.value(a0)]
 
 # Objective function
-def f_x(alpha, beta, gamma, a0, w0, theta0, T, gate_x, gate_y):
+def f1_x(alpha, beta, gamma, a0, w0, theta0, traj, gate_x, gate_y):
+    # MAX_TIME_ROUNDS = len(traj.time)
+
     res = 0
-    dt = T / ROUNDS
-    for i in range(1, ROUNDS + 1):
-        dx = (gate_x - x_pos(i * dt))
-        if dx == 0:
-            res += (theta(alpha, beta, gamma, a0, w0, theta0, i * dt) - pi / 2) ** 2
+    for i in range(MAX_TIME_ROUNDS):
+        dx = (gate_x - traj.p_x[i])
+        dy = (gate_y - traj.p_y[i])
+        if dx == 0 and dy == 0:
+            dx = (gate_x - traj.p_x[i - 1])
+            dy = (gate_y - traj.p_y[i - 1])
+            res += (theta(alpha, beta, gamma, a0, w0, theta0, traj.time[i]) - pi / 2) ** 2
+        elif dx == 0 and dy > 0:
+            res += (theta(alpha, beta, gamma, a0, w0, theta0, traj.time[i]) - pi / 2) ** 2
+        elif dx == 0 and dy < 0:
+            res += (theta(alpha, beta, gamma, a0, w0, theta0, traj.time[i]) + pi / 2) ** 2
         else:
-            res += (theta(alpha, beta, gamma, a0, w0, theta0, i * dt) - atan((gate_y - y_pos(i * dt)) / dx)) ** 2
+            res += (theta(alpha, beta, gamma, a0, w0, theta0, traj.time[i]) - atan(dy / dx)) ** 2
     return res
+
+def f2_x(alpha, beta, gamma, a0, w0, theta0, traj, gate_x, gate_y):
+    # MAX_TIME_ROUNDS = len(traj.time)
+
+    res = 0
+    for i in range(MAX_TIME_ROUNDS):
+        dx = (gate_x - traj.p_x[i])
+        dy = (gate_y - traj.p_y[i])
+        ds = sqrt(dx ** 2 + dy ** 2)
+        if ds == 0:
+            res += 0
+        else:
+            # res += (sin(theta(alpha, beta, gamma, a0, w0, theta0, traj.time[i]) - acos(dx / ds)) ** 2) * 100 * i
+            res += (theta(alpha, beta, gamma, a0, w0, theta0, traj.time[i]) - asin(dy / ds)) ** 2 * i
+        # if dx == 0 and dy == 0:
+        #     dx = (gate_x - traj.p_x[i - 1])
+        #     dy = (gate_y - traj.p_y[i - 1])
+        #     res += (theta(alpha, beta, gamma, a0, w0, theta0, traj.time[i]) - pi / 2) ** 2
+        # elif dx == 0 and dy > 0:
+        #     res += (theta(alpha, beta, gamma, a0, w0, theta0, traj.time[i]) - pi / 2) ** 2
+        # elif dx == 0 and dy < 0:
+        #     res += (theta(alpha, beta, gamma, a0, w0, theta0, traj.time[i]) + pi / 2) ** 2
+        # else:
+        #     res += (theta(alpha, beta, gamma, a0, w0, theta0, traj.time[i]) - atan(dy / dx)) ** 2
+    return res
+
+        
+
 
 #Limitations
 def g1_x(alpha, beta, gamma, a0, w0, t):
@@ -116,14 +199,16 @@ def acc(alpha, beta, gamma, a0, t):
     res = alpha * (t ** 3) / 6 + beta * (t ** 2) / 2 + gamma * t + a0
     return res
     
-def plot_yaw(alpha, beta, gamma, a0, w0, theta0, T, gate_x, gate_y):
+def plot_yaw(alpha, beta, gamma, a0, w0, theta0, traj, gate):
+    rounds = len(traj.time)
+
     print("alpha: ", alpha)
     print("beta: ", beta)
     print("gamma: ", gamma)
     print("a0: ", a0)
     print()
 
-    x_axis = []
+    x_axis = traj.time
     y_axis = []
     target = []
     path_t = []
@@ -131,24 +216,32 @@ def plot_yaw(alpha, beta, gamma, a0, w0, theta0, T, gate_x, gate_y):
     angular_velocity = []
     map_x = []
     map_y = []
-    for i in range(1, ROUNDS + 1):
-        dt = T / ROUNDS
-        x_axis.append(i * dt)
-        y_axis.append(theta(alpha, beta, gamma, a0, w0, theta0, i * dt) * 180 / pi)
-        angular_velocity.append(omega(alpha, beta, gamma, a0, w0, i * dt))
-        acc_axis.append(acc(alpha, beta, gamma, a0, i * dt))
-        map_x.append(x_pos(i * dt))
-        map_y.append(y_pos(i * dt))
-        dx = (gate_x - x_pos(i * dt))
-        if dx == 0:
-            target.append(90)
+    path_t_last = -90
+
+    for i in range(rounds):
+        # x_axis.append(i * dt)
+        y_axis.append(theta(alpha, beta, gamma, a0, w0, theta0, traj.time[i]) * 180 / pi)
+        angular_velocity.append(omega(alpha, beta, gamma, a0, w0, traj.time[i]))
+        acc_axis.append(acc(alpha, beta, gamma, a0, traj.time[i]))
+        map_x.append(traj.p_x[i])
+        map_y.append(traj.p_y[i])
+        dx = (gate.x - traj.p_x[i])
+        dy = (gate.y - traj.p_y[i])
+        ds = sqrt(dx ** 2 + dy ** 2)
+        if ds != 0:
+            target.append(asin(dy / ds) * 180 / pi)
         else:
-            target.append(atan((gate_y - y_pos(i * dt)) / dx) * 180 / pi)
-        dx = (x_pos(i * dt) - x_pos((i - 1) * dt))
-        if dx == 0:
-            path_t.append(90)
+            target.append(target[-1])
+        
+        if i != rounds - 1:
+            dx = (traj.p_x[i + 1] - traj.p_x[i])
+            dy = (traj.p_y[i + 1] - traj.p_y[i])
+            ds = sqrt(dx ** 2 + dy ** 2)
+            if ds != 0:
+                path_t_last = asin(dy / ds) * 180 / pi
+            path_t.append(path_t_last)
         else:
-            path_t.append(atan((y_pos(i * dt) - y_pos((i - 1) * dt)) / dx) * 180 / pi)
+            path_t.append(path_t_last)
 
     plt.subplot(221)
     plt.plot(x_axis, y_axis, 'r', label = "Planned Yaw")
@@ -174,20 +267,23 @@ def plot_yaw(alpha, beta, gamma, a0, w0, theta0, T, gate_x, gate_y):
 
     plt.subplot(224)
     plt.plot(map_x, map_y)
-    plt.scatter(gate_x, gate_y, color = 'r', s = 100)
+    plt.scatter(gate.x, gate.y, color = 'r', s = 100)
     plt.title("MAP")
 
     plt.show()
 
-# Trajectory
-def x_pos(t):
-    if t < 0.125:
-        return 0
-    else:
-        return 4 - 4 * cos((t - 0.125) * 2)
+    print(pi)
+    print(y_axis[-1])
 
-def y_pos(t):
-    if t < 0.125:
-        return t / 8
-    else:
-        return 4 * sin((t - 0.125) * 2) + 1
+# Trajectory
+# def x_pos(t):
+#     if t < 0.125:
+#         return 0
+#     else:
+#         return 4 - 4 * cos((t - 0.125) * 2)
+
+# def y_pos(t):
+#     if t < 0.125:
+#         return t / 8
+#     else:
+#         return 4 * sin((t - 0.125) * 2) + 1
