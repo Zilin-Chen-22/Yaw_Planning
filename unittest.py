5ac8b568c4dca0db5630e4ee957a40f0d59fad1a
Zilin <3485087383@qq.com>
2024-12-19 00:51:40 -0500

    Update file

--- unittest.py
+++ unittest.py
@@ -2,7 +2,7 @@ from csv_reader import *
 from define import *
 
 for i in range(1, 100):
-    filename = "/home/zilin/Documents/python_file/racing_data/2_time_trial_data/bitmatta/flight_%d_pilot_bitmatta.csv" % (i)
+    filename = "../racing_data/2_time_trial_data/bitmatta/flight_%d_pilot_bitmatta.csv" % (i)
 
     pilot = read_human_csv(filename)
 
